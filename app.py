189909f1d1f3210daa23eea2d38dfc39696f77ed
Ajeszny <245107@edu.p.lodz.pl>
2025-11-15 10:35:20 +0100

    Add app.py entrypoint for S2I

---
+++ app.py
@@ -0,0 +1,8 @@
+# app.py (root of repo)
+
+import uvicorn
+from app.main import app  # this imports the FastAPI instance from app/main.py
+
+if __name__ == "__main__":
+    # S2I Python image will run: python app.py
+    uvicorn.run(app, host="0.0.0.0", port=8080)
